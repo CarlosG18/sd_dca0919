dbb05220720f8ed39223562800b7d94a3d033928
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-12-12 22:08:49 -0300

    add cod main.vhdl

--- projetos/uni3/codigos/main.vhdl
+++ projetos/uni3/codigos/main.vhdl
@@ -1,52 +1,101 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity Portao is
+entity PROJETO_SD is
     port(
         clk : in std_logic;
-        btn_start: in std_logic;
-        btn_config: in std_logic;
-        btn_portao : in std_logic;
-        leds_portao : out std_logic_vector(17 downto 0);
-        estado_ant: out std_logic_vector(3 downto 0);
+        botao : in std_logic;
+        switch_vel: in std_logic;
+        ea: in std_logic;
+        aberto_fechado : in std_logic;
+        leds : out std_logic_vector(17 downto 0);
     );
-end Portao;
+end PROJETO_SD;
 
-architecture behaviorial of Portao is
-    type estados is (FECHADO, ABRINDO, PARADO, ABERTO, FECHANDO, CONFIG);
+architecture behaviorial of PROJETO_SD is
+    type estados is (FECHADO_NP,FECHADO_P,FECHANDO_P,FECHANDO_NP,ABRINDO_NP,ABRINDO_P, PARADO_P,PARADO_NP, ABERTO_NP,ABERTO_P, CONFIG_P,CONFIG_NP);
     signal estado: estados;
 begin
-    process(clk)
-    begin
-        if (clk'event and clk='1') and btn_start = '1' then
-            estado <= FECHADO;
-            case estado is
-                when FECHADO =>
-                    leds_portao <= "111111111111111111";
-                    if btn_config = '1' then
-                        estado <= CONFIG;
-                        estado_ant <= "000";
-                    elsif btn_portao = '1' then 
-                        estado <= ABRINDO;
-                    end if;
-                when ABRINDO =>
-                    if btn_portao = '1' then
-                        estado <= PARADO;
-                    elsif leds_portao = "000000000000000000" then
-                        estado <= ABERTO;
-                    end if;
-                    
-                when PARADO =>
-                    if btn_portao = '1' then 
-                        estado <= ABRINDO;
-                    end if;
-                when ABERTO =>
-                        
-                when FECHANDO =>
-                    
-                when CONFIG => 
-                    
-            end case;
-        end if;
-    end process;
+  process(clk)
+  begin 
+    if clk'event and clock = '1' then
+      case estado is
+        when FECHADO_NP =>
+          if botao = '1' then
+            estado <= ABRINDO_P;
+          elsif switch_vel = '1' then
+            estado <= CONFIG_NP;
+          end if;
+        when ABRINDO_P =>
+          if leds = "000000000000000000" and botao = '1' then
+            estado <= ABERTO_P;
+          elsif botao = '0' then
+            estado <= ABRINDO_NP;
+          elsif botao = '1':
+            estado <= ABRINDO_P;
+          end if;
+        when ABERTO_P =>
+          if switch_vel = '1' then
+            estado <= CONFIG_P;
+          elsif botao = '1' then 
+            estado <= ABERTO_P;
+          elsif botao = '0' then
+            estado <= ABERTO_NP;
+          end if;
+        when ABERTO_NP => 
+          if switch_vel = '1' then
+            estado <= CONFIG_NP;
+          elsif botao = '1' then
+            estado <= FECHANDO_P;
+          else then
+            estado <= ABERTO_NP;
+          end if;
+        when FECHANDO_P =>
+          if botao = '1' then
+            estado <= FECHANDO_P;
+          else then
+            estado <= FECHANDO_NP;
+          end if;
+        when FECHADO_NP =>
+          ea <= '0';
+          if leds = '111111111111111111' then 
+            estado <= FECHADO_P;
+          elsif botao = '1' then
+            estado <= PARADO_P;
+          else then 
+            estado <= FECHADO_NP;
+          end if;
+        when PARADO_P =>
+          if botao = '1' then
+            estado <= PARADO_P;
+          else then 
+            estado <= PARADO_NP;
+          end if;
+        when PARADO_NP =>
+          if botao = '0' then
+            estado <= PARADO_NP;
+          elsif botao = '1' and ea = '0' then
+            estado <= ABRINDO_NP;
+          else then
+            estado <= FECHANDO_P;
+          end if;
+        when CONFIG_P =>
+          if switch_vel = '0' and leds = '111111111111111111' then 
+            estado <= FECHADO_P;
+          elsif botao = '1' then
+            estado <= CONFIG_P;
+          else then 
+            estado <= CONFIG_NP;
+          end if;
+        when CONFIG_NP =>
+          if switch_vel = '0' and leds = '111111111111111111' then 
+            estado <= FECHADO_NP;
+          elsif switch_vel = '0' and leds = '000000000000000000' then 
+            estado <= ABERTO_NP;
+          elsif switch_vel = '1' then 
+            estado <= CONFIG_NP;
+          end if;
+      end case;
+    end if;
+  end process;
 end behaviorial;
